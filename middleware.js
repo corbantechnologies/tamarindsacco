244d1f63e3a6b4b823a4fc75aa433c86a91e7ede
dalienst <oduordalienst@gmail.com>
2025-09-14 14:48:32 +0300

    remove middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,10 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
-export const config = {
-  matcher: [
-    "/sacco-admin/:path*",
-    "/member/:path*",
-    // "/superuser/:path*",
-    // "/members/:path*",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/sacco-admin/:path*",
+//     "/member/:path*",
+//     // "/superuser/:path*",
+//     // "/members/:path*",
+//   ],
+// };
