c119a0cc6bc0575f5febc0fd36ca9c17a1194c2d
dalienst <oduordalienst@gmail.com>
2025-09-15 13:33:47 +0300

    fetch key factors

--- app/(private)/member/dashboard/page.jsx
+++ app/(private)/member/dashboard/page.jsx
@@ -1,8 +1,33 @@
 "use client";
 
+import MemberLoadingSpinner from "@/components/general/MemberLoadingSpinner";
+import { useFetchMember } from "@/hooks/members/actions";
+import { useFetchSavings } from "@/hooks/savings/actions";
+import { useFetchSavingsTypes } from "@/hooks/savingtypes/actions";
 import React from "react";
 
 function MemberDashboard() {
+  const {
+    isLoading: isLoadingMember,
+    data: member,
+    refetch: refetchMember,
+  } = useFetchMember();
+
+  const {
+    isLoading: isLoadingSavingTypes,
+    data: savingTypes,
+    refetch: refetchSavingTypes,
+  } = useFetchSavingsTypes();
+
+  const {
+    isLoading: isLoadingSavings,
+    data: savings,
+    refetch: refetchSavings,
+  } = useFetchSavings();
+
+  if (isLoadingMember || isLoadingSavingTypes || isLoadingSavings)
+    return <MemberLoadingSpinner />;
+
   return <div>MemberDashboard</div>;
 }
 
