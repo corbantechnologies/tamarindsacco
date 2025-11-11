585a6edd5ebae046fcbc58b83642246695bb19af
Dalienst Owino <oduordalienst@gmail.com>
2025-11-11 21:33:01 +0300

    fetch

--- hooks/transactions/actions.jsx
+++ hooks/transactions/actions.jsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { getAccountsList } from "@/services/transactions";
+import {
+  getAccountsList,
+  getMemberYearlySummary,
+} from "@/services/transactions";
 import useAxiosAuth from "../authentication/useAxiosAuth";
 import { useQuery } from "@tanstack/react-query";
+import useMemberNo from "../authentication/useMemberNo";
 
 export function useFetchAccountsList() {
   const token = useAxiosAuth();
@@ -11,5 +15,16 @@ export function useFetchAccountsList() {
     queryKey: ["accountsList"],
     queryFn: () => getAccountsList(token),
     enabled: !!token,
+  });
+}
+
+export function useFetchMemberYearlySummary() {
+  const token = useAxiosAuth();
+  const member_no = useMemberNo();
+
+  return useQuery({
+    queryKey: ["summary", member_no],
+    queryFn: () => getMemberYearlySummary(member_no, token),
+    enabled: !!token && !!member_no,
   });
 }
